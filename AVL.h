adca5eab54be8219fe01bd9d5f79caf9511da35c
Evap1 <101967009+Evap1@users.noreply.github.com>
2023-05-20 17:20:41 +0300

    made the tree more "secure" from un-supervised access. made some better order and readability. made encapsulation: 1.  added root field to be initialized once. 2.  deleted setters. 3.  added insert/remove public methods to be accessed only via a tree. updates the whole tree. 4.  everything related to rotations or setters moved to private section.

--- AVL.h
+++ AVL.h
@@ -9,28 +9,37 @@
 using namespace std;
 
 //TODO: test this whole thing
-//TBD operator = not really because it's pointers
-// AVL TreeNode
+//TODO: operator = not really because it's pointers
+
+// AVL Tree
 template <class T>
 class TreeNode
 {
 public:
-    explicit TreeNode(const T& value);
     ~TreeNode();
 
-    //getters
+    // Getters
     int getHeight();
     TreeNode* getLeft();
     TreeNode* getRight();
     T getKey();
+
+    // Only visible operations:
+    void insert(const T& value);
+    void remove(const T& value);
+
+private:
+    TreeNode();
+    T key;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode *root;
+    int height;
+    int weight;
+
+    // Rotation methods
     int get_balance_factor(TreeNode *current);
-
-    //setters
-    void setLeft(TreeNode* other);
-    void setRight(TreeNode* other);
     void updateHeight();
-
-    //Rotation methods
     TreeNode* right_rotate(TreeNode* B);
     TreeNode* left_rotate(TreeNode* A);
     TreeNode* rotate_LL(TreeNode *v);
@@ -38,110 +47,29 @@ public:
     TreeNode* rotate_LR(TreeNode *v);
     TreeNode* rotate_RR(TreeNode *v);
 
-    //additional methods
+    // additional methods
     TreeNode* insert(TreeNode* node, const T& value);
     TreeNode* find(const T& value);
     TreeNode* findMin(TreeNode* v);
     TreeNode* remove(const T& value, TreeNode *v);
-
-private:
-
-    T key;
-    TreeNode *left;
-    TreeNode *right;
-    int height;
-    int weight;
 };
 
-template<class T>
-TreeNode<T> *TreeNode<T>::findMin(TreeNode *v) {
-    if ( v == NULL){
-        return NULL;
-    }
-    //min is only on sub left tree
-    else if (v->left == NULL){
-        return v;
-    }
-    //min will be found on subtree on the left only
-    else return findMin(v->left);
-}
+// PRIVATE METHODS:
 
-template<class T>
-TreeNode<T>* TreeNode<T>::remove(const T& value, TreeNode *v) {
-    TreeNode* temp;
-
-    //if value not found, remove returns NULL.
-    if (v == NULL){
-        return NULL;
-    }
-
-    //binary search for the element. ALL CASES ONLY FOR FOUND ELEMENT
-    //case1: value to be found is smaller than current value
-    else if ( value < v->key){
-        v->left = remove(value, v->left);
-    }
-    //case2: value to be found is greater than current value
-    else if ( value > v->key){
-        v->right = remove(value, v->right);
-    }
-    //case3.1: element is found with two subtrees
-    else if (v->left != NULL && v->right != NULL){
-        //find the new head of the right subtree
-        temp = findMin(v->right);
-        //copy the min element in right subtree
-        v->key = temp->key;
-        //delete other(!) element which we have just copied.
-        v->right = remove(v->key, v->right);
-    }
-    //case3.2: element is found with one or none subtrees
-    else {
-        temp = v;
-        if (v->left == NULL){
-            v = v->right;
-        }
-        else if (v->right == NULL){
-            v = v->left;
-        }
-        delete temp;
-    }
-    //in case v is the last on any subtree;
-    if ( v == NULL){
-        return v;
-    }
-    //update new heights:
-    v->updateHeight();
-
-    //recursively rotations if needed, from bottom -> up:
-    //case1: if left node is deleted, right is heavier, rotation to left
-    if (get_balance_factor(v) == 2){
-        if (get_balance_factor(v->left) == -1){
-            return rotate_LR(v);
-        }
-        else return rotate_LL(v);
-    }
-    //case2: if right node is deleted, left is heavier, rotation to right
-    else if (get_balance_factor(v) == -2){
-        if (get_balance_factor(v->right) == 1){
-            return rotate_RL(v);
-        }
-        else return rotate_RR(v);
-    }
-    return v;
-}
-
-//constructor
+// private constructor to be accessed once: defines our root to be empty.
 template <class T>
-TreeNode<T>::TreeNode(const T& value) :
-        key(value),
+TreeNode<T>::TreeNode() :
+        key(NULL),
         left(NULL),
         right(NULL),
+        root(NULL),
         height(0),
-        weight(1)
+        weight(0)
 {}
 
-//destructor
-//ATTENTION may not work, make sure valngrid
-//maybe void recursion
+// Destructor
+// ATTENTION may not work, make sure valngrid
+// maybe void recursion
 template <class T>
 TreeNode<T>::~TreeNode(){
     if (left != NULL){
@@ -152,18 +80,8 @@ TreeNode<T>::~TreeNode(){
     }
 }
 
-template<class T>
-void TreeNode<T>::setRight(TreeNode *other) {
-    right = other;
-}
-
-template<class T>
-void TreeNode<T>::setLeft(TreeNode *other) {
-    left = other;
-}
-
-//dealing with possibility that we are too low in the tree
-//works as a setter
+// dealing with possibility that we are too low in the tree
+// works as a setter
 template<class T>
 void TreeNode<T>::updateHeight() {
     if (left == NULL){
@@ -178,27 +96,6 @@ void TreeNode<T>::updateHeight() {
     height = 1 + max(left->height, right->height);
 }
 
-template<class T>
-int TreeNode<T>::getHeight() {
-    if (this == NULL) return -1;
-    return height;
-}
-
-template<class T>
-TreeNode<T> *TreeNode<T>::getLeft() {
-    return left;
-}
-
-template<class T>
-TreeNode<T> *TreeNode<T>::getRight() {
-    return right;
-}
-
-template<class T>
-T TreeNode<T>::getKey() {
-    return key;
-}
-
 // Get the balance factor of a node
 template<class T>
 int TreeNode<T>::get_balance_factor(TreeNode *current)
@@ -211,7 +108,7 @@ int TreeNode<T>::get_balance_factor(TreeNode *current)
     return l-r;
 }
 
-//Method to determine whether some value already exist
+// Method to determine whether some value already exist
 template<class T>
 TreeNode<T>* TreeNode<T>::find(const T &value) {
     if (this == NULL) return NULL;
@@ -227,7 +124,7 @@ TreeNode<T>* TreeNode<T>::find(const T &value) {
     return find(value);;
 }
 
-//Insert new node as a leaf and make rotations if needed to remain balanced tree
+// Insert new node as a leaf and make rotations if needed to remain balanced tree- all the way to the root.
 template<class T>
 TreeNode<T> *TreeNode<T>::insert(TreeNode* node, const T& value) {
     if (node == NULL){
@@ -257,20 +154,97 @@ TreeNode<T> *TreeNode<T>::insert(TreeNode* node, const T& value) {
     updateHeight(node);
     return node;
 }
+// Find min value and return it's node.
+template<class T>
+TreeNode<T> *TreeNode<T>::findMin(TreeNode *v) {
+    if ( v == NULL){
+        return NULL;
+    }
+        //min is only on sub left tree
+    else if (v->left == NULL){
+        return v;
+    }
+        //min will be found on subtree on the left only
+    else return findMin(v->left);
+}
 
-//When lowest node is on the LL path form v
+// Delete a node and re-balance the tree - all the way to the root.
+template<class T>
+TreeNode<T>* TreeNode<T>::remove(const T& value, TreeNode *v) {
+    TreeNode* temp;
+
+    // if value not found, remove returns NULL.
+    if (v == NULL){
+        return NULL;
+    }
+
+        // binary search for the element. ALL CASES ONLY FOR FOUND ELEMENT
+        // case1: value to be found is smaller than current value
+    else if ( value < v->key){
+        v->left = remove(value, v->left);
+    }
+        // case2: value to be found is greater than current value
+    else if ( value > v->key){
+        v->right = remove(value, v->right);
+    }
+        // case3.1: element is found with two subtrees
+    else if (v->left != NULL && v->right != NULL){
+        //find the new head of the right subtree
+        temp = findMin(v->right);
+        //copy the min element in right subtree
+        v->key = temp->key;
+        //delete other(!) element which we have just copied.
+        v->right = remove(v->key, v->right);
+    }
+        // case3.2: element is found with one or none subtrees
+    else {
+        temp = v;
+        if (v->left == NULL){
+            v = v->right;
+        }
+        else if (v->right == NULL){
+            v = v->left;
+        }
+        delete temp;
+    }
+    //in case v is the last on any subtree;
+    if ( v == NULL){
+        return v;
+    }
+    //update new heights:
+    v->updateHeight();
+
+    // recursively rotations if needed, from bottom -> up:
+    // case1: if left node is deleted, right is heavier, rotation to left
+    if (get_balance_factor(v) == 2){
+        if (get_balance_factor(v->left) == -1){
+            return rotate_LR(v);
+        }
+        else return rotate_LL(v);
+    }
+        // case2: if right node is deleted, left is heavier, rotation to right
+    else if (get_balance_factor(v) == -2){
+        if (get_balance_factor(v->right) == 1){
+            return rotate_RL(v);
+        }
+        else return rotate_RR(v);
+    }
+    return v;
+}
+
+// When lowest node is on the LL path from v.
 template<class T>
 TreeNode<T> *TreeNode<T>::rotate_LL(TreeNode *v) {
     return right_rotate(v) ;
 }
 
-//When lowest node is on the RR path form v
+// When lowest node is on the RR path from v.
 template<class T>
 TreeNode<T> *TreeNode<T>::rotate_RR(TreeNode *v) {
     return left_rotate(v) ;
 }
 
-//When lowest node is on the RL path form v
+// When lowest node is on the RL path from v.
 template<class T>
 TreeNode<T> *TreeNode<T>::rotate_RL(TreeNode *v) {
     TreeNode* vr = v->right;
@@ -278,7 +252,7 @@ TreeNode<T> *TreeNode<T>::rotate_RL(TreeNode *v) {
     return left_rotate(v) ;
 }
 
-//When lowest node is on the LR path form v
+// When lowest node is on the LR path from v.
 template<class T>
 TreeNode<T> *TreeNode<T>::rotate_LR(TreeNode *v) {
     TreeNode* vl = v->left;
@@ -286,7 +260,7 @@ TreeNode<T> *TreeNode<T>::rotate_LR(TreeNode *v) {
     return right_rotate(v) ;
 }
 
-//performing left rotation when A was the src root and then B is the final root.
+// performing left rotation when A was the src root and then B is the final root.
 template<class T>
 TreeNode<T> *TreeNode<T>::left_rotate(TreeNode *A) {
     //Sub-trees and nodes to be modified
@@ -305,7 +279,7 @@ TreeNode<T> *TreeNode<T>::left_rotate(TreeNode *A) {
     return B;
 }
 
-//performing right rotation when B was the src root and then A is the final root.
+// performing right rotation when B was the src root and then A is the final root.
 template<class T>
 TreeNode<T> *TreeNode<T>::right_rotate(TreeNode *B) {
     //Sub-trees and nodes to be modified
@@ -324,10 +298,44 @@ TreeNode<T> *TreeNode<T>::right_rotate(TreeNode *B) {
     return A;
 }
 
-// Function to get the maximum of two integers
+// Function to get the maximum of two integers.
 int max(int a, int b){
     return a > b ? a : b ;
 }
 
+
+// PUBLIC METHODS:
+template<class T>
+void TreeNode<T>::remove(const T &value) {
+    root = remove(value,root);
+}
+
+template<class T>
+void TreeNode<T>::insert(const T &value) {
+    root = insert(value, root);
+}
+
+
+// Getters:
+template<class T>
+int TreeNode<T>::getHeight() {
+    if (this == NULL) return -1;
+    return height;
+}
+
+template<class T>
+TreeNode<T> *TreeNode<T>::getLeft() {
+    return left;
+}
+
+template<class T>
+TreeNode<T> *TreeNode<T>::getRight() {
+    return right;
+}
+
+template<class T>
+T TreeNode<T>::getKey() {
+    return key;
+}
 
 #endif //WET1_AVL_H
