d84c594471155b1b7cca53c1c0066b523aa96aa3
Galor Lazar <galor.lazar@campus.technion.ac.il>
2023-05-25 23:16:31 +0300

    Adding Movie class initialisation and it's common functions.

--- Movie.cpp
+++ Movie.cpp
@@ -1,5 +1,99 @@
-//
-// Created by Eva Poluliakhov on 13/05/2023.
-//
 
 #include "Movie.h"
+
+
+
+
+
+//________________________Constructor_________________________
+
+// Consumer must check if ID already exists
+Movie::Movie(int id, Genre genreType, int viewsCount, bool isVip) : 
+    movieId(id), genre(genreType), views(viewsCount), vipOnly(isVip)
+{
+    rate = 0;
+    ratersCont = 0;
+}
+
+
+
+ // _______________________________________________Functions______________________________________________
+
+/// @brief add views to the movie's views count
+/// @param count assume that count is positive
+void Movie::add_views(int count)
+{
+    if (count >= 0)
+        views += count;
+}
+
+
+/// @brief adds one more user's rating to the movie's rating calculation
+/// @param rate assume that in range 1-100
+void Movie::add_rating(int rated)
+{
+    if (rated > 0 && rated < 100)
+    {
+        rate = (ratersCont * rate + rated) / (ratersCont + 1);
+        ratersCont++;
+    }
+}
+
+
+
+
+// _________________________________________________GETTERS______________________________________________________________
+
+int Movie::getMovieId() const{
+    return movieId;
+}
+
+Genre Movie::getGenre() const{
+    return genre;
+}
+
+bool Movie::isVipOnly() const{
+    return vipOnly;
+}
+
+int Movie::getViews() const{
+    return views;
+}
+
+double Movie::getRate() const{
+    return rate;
+}
+
+
+
+
+
+// ___________________________________________Operator Overloading__________________________________________
+
+
+bool operator>(const Movie &movie1, const Movie &movie2)
+{
+
+    if(movie1.getRate() > movie2.getRate())
+        return true;
+    else if (movie1.getRate() == movie2.getRate())
+    {
+        if (movie1.getViews() > movie2.getViews())
+            return true;
+        else if (movie1.getViews() == movie2.getViews())
+        {
+            if (movie1.getMovieId() > movie2.getMovieId())
+                return true;
+        }
+    }
+
+    return false;
+}
+
+bool operator<(const Movie& movie1, const Movie& movie2)
+{
+    return (movie2 > movie1);
+}
+
+
+
--- Movie.h
+++ Movie.h
@@ -1,11 +1,35 @@
-//
-// Created by Eva Poluliakhov on 13/05/2023.
-//
 
 #ifndef WET1_MOVIE_H
 #define WET1_MOVIE_H
 
-class Movies{
+#include "wet1util.h"
+
+
+class Movie{
+
+    private:
+    
+    int movieId;
+    Genre genre;
+    bool vipOnly;
+    int views;
+    double rate;
+    int ratersCont;
+
+public:
+    Movie(int id, Genre genreType, int viewsCount, bool isVip);
+    ~Movie() = default;
+
+    int getMovieId() const;
+    Genre getGenre() const;
+    bool isVipOnly() const;
+    int getViews() const;
+    double getRate() const;
+
+    void add_views(int count);
+    void add_rating(int rate);
+
+
 
 };
 
