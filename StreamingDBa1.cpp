e143b0c24477d38b9a1a58370386b8e469a848d9
Evap1 <101967009+Evap1@users.noreply.github.com>
2023-05-27 23:10:42 +0300

    merged both Eva andGalor branches to main. was a conflict with Galor's branch so marged manually.

--- StreamingDBa1.cpp
+++ StreamingDBa1.cpp
@@ -1,4 +1,6 @@
 #include "StreamingDBa1.h"
+#include "AVL.h"
+StatusType correct_status(StatusType status1,StatusType status2);
 
 streaming_database::streaming_database()
 {
@@ -13,14 +15,46 @@ streaming_database::~streaming_database()
 
 StatusType streaming_database::add_movie(int movieId, Genre genre, int views, bool vipOnly)
 {
-	// TODO: Your code goes here
-	return StatusType::SUCCESS;
+    if(movieId <= 0 || genre == Genre::NONE ||  views < 0)
+        return StatusType::INVALID_INPUT;
+
+    Movie* m = new Movie(movieId,genre,views, vipOnly);
+    if(moviesByGenre[(int)Genre::NONE]->find(*m) == NULL)
+    {
+        StatusType status1 = moviesByGenre[(int)genre]->insert(*m);
+        StatusType status2 = moviesByGenre[(int)Genre::NONE]->insert(*m);
+        return correct_status(status1, status2);
+
+    }
+
+
+
+    /// TODO: insert must return status type
+    return StatusType::FAILURE;
+
+
+    // TODO: Your code goes here
+    //return StatusType::SUCCESS;
 }
 
 StatusType streaming_database::remove_movie(int movieId)
 {
-	// TODO: Your code goes here
-	return StatusType::SUCCESS;
+    if(movieId <=0)
+        return StatusType::INVALID_INPUT;
+    Movie* m = new Movie(movieId,Genre::NONE,0, false);
+
+    if(moviesByGenre[(int)Genre::NONE]->find(*m) != NULL)
+    {
+        StatusType status1 = moviesByGenre[(int)genre]->remove(*m);
+        StatusType status2 = moviesByGenre[(int)Genre::NONE]->remove(*m);
+        return correct_status(status1, status2);
+
+    }
+
+    /// TODO: remove must return status type
+    return moviesByGenre[(int)Genre::NONE]->remove(*m);
+
+    // TODO: Your code goes here
 }
 
 /// @brief add new user to users tree.
@@ -32,6 +66,7 @@ StatusType streaming_database::add_user(int userId, bool isVip)
 	if (userId <= 0) return StatusType::INVALID_INPUT;
     return users.insert(User(userId,isVip));
 }
+
 /// @brief remove existing user from users tree.
 /// @param userId
 /// @return Status of the operation
@@ -40,6 +75,7 @@ StatusType streaming_database::remove_user(int userId)
     if (userId <= 0) return StatusType::INVALID_INPUT;
 	return users.remove(User(userId));
 }
+
 /// @brief add an empty new group to groups tree.
 /// @param groupId
 /// @return Status of the operation
@@ -48,6 +84,7 @@ StatusType streaming_database::add_group(int groupId)
     if (groupId <= 0) return StatusType::INVALID_INPUT;
 	return groups.insert(Group(groupId));
 }
+
 /// @brief
 /// @param groupId
 /// @return
@@ -80,17 +117,38 @@ StatusType streaming_database::group_watch(int groupId,int movieId)
 
 output_t<int> streaming_database::get_all_movies_count(Genre genre)
 {
-    // TODO: Your code goes here
-    static int i = 0;
-    return (i++==0) ? 11 : 2;
+
+    if(moviesByGenre[(int)genre] != NULL)
+    {
+        return get_all_movies(moviesByGenre[(int)genre], output);
+
+    }
+
+    // // TODO: Your code goes here
+    // output[0] = 4001;
+    // output[1] = 4002;
+    return StatusType::SUCCESS;
 }
 
 StatusType streaming_database::get_all_movies(Genre genre, int *const output)
 {
-    // TODO: Your code goes here
-    output[0] = 4001;
-    output[1] = 4002;
-    return StatusType::SUCCESS;
+
+    /// TODO: create a get left and get right function to the tree
+    if (moviesTree == NULL){
+        return;
+    }
+
+    if (moviesTree->get){
+        node->right = insert(node->right, value);
+    }
+
+    if (node->key > value){
+        node->left = insert(node->left, value);
+    }
+
+    node = balance_Tree(node);
+    updateHeight(node);
+    return node;
 }
 
 output_t<int> streaming_database::get_num_views(int userId, Genre genre)
@@ -112,4 +170,24 @@ output_t<int> streaming_database::get_group_recommendation(int groupId)
     return (i++==0) ? 11 : 2;
 }
 
+/// @brief Checks what a function needs to return if there are more than one status type
+/// @param status1
+/// @param status2
+/// @return
+StatusType correct_status(StatusType status1,StatusType status2)
+{
+    if(status1 == StatusType::SUCCESS && status2 == StatusType::SUCCESS)
+        return StatusType::SUCCESS;
 
+    if(status1 == StatusType::ALLOCATION_ERROR || status2 == StatusType::ALLOCATION_ERROR)
+        return StatusType::ALLOCATION_ERROR;
+
+    if(status1 == StatusType::INVALID_INPUT || status2 == StatusType::INVALID_INPUT)
+        return StatusType::INVALID_INPUT;
+
+    if(status1 == StatusType::FAILURE || status2 == StatusType::FAILURE)
+        return StatusType::FAILURE;
+
+    return StatusType::FAILURE;
+
+}
